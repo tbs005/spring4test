4f3a1120454a352497cd72ae83f06a8c182320f4
王炜华 <weihua.wang@gewara.com>
2017-07-11 11:36:04 +0800

    添加异常处理器

---
+++ src/main/java/com/spring4/webtest/config/WebAppExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.spring4.webtest.config;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * 处理web应用控制器通用异常
+ *
+ */
+@ControllerAdvice
+public class WebAppExceptionHandler {
+}
