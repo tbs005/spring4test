05f39bb69b29c4462edacb59966971fb923183ee
王炜华 <weihua.wang@gewara.com>
2017-07-25 16:55:18 +0800

    添加类注释

--- src/main/java/com/spring4/webtest/config/DBConfig.java
+++ src/main/java/com/spring4/webtest/config/DBConfig.java
@@ -7,7 +7,12 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * 数据源配置
- * H2 数据库使用参考：http://www.cnblogs.com/xdp-gacl/p/4171024.html
+ * H2 数据库使用参考：
+ * http://www.cnblogs.com/xdp-gacl/p/4171024.html
+ * http://www.cnblogs.com/love540376/p/5311907.html
+ * http://www.cnblogs.com/jason0529/p/6529880.html
+ * http://blog.csdn.net/zhangjq520/article/details/53931003
+ * 
  */
 @Configuration
 @ComponentScan(basePackages = "com.spring4.webtest.dao")
