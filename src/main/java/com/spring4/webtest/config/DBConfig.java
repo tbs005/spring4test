5f282754eb39942257bdfa7674e446c073b949e4
王炜华 <weihua.wang@gewara.com>
2017-07-27 17:45:50 +0800

    JPA配置修改

--- src/main/java/com/spring4/webtest/config/DBConfig.java
+++ src/main/java/com/spring4/webtest/config/DBConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -28,7 +29,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * 
  */
 @Configuration
-@ComponentScan(basePackages = "com.spring4.webtest.dao")
+//@ComponentScan(basePackages = {"com.spring4.webtest.dao","com.spring4.webtest.domain"})
+@EnableJpaRepositories(basePackages = {"com.spring4.webtest.dao","com.spring4.webtest.domain"})
 @EnableTransactionManagement
 public class DBConfig {
 	
